0b11c47480f1b12dfc1a32b5ef60f9476f545b24
Alexander Menk <a.menk@imi.de>
2023-10-08 14:11:35 +0200

    Adapt English text in Amharic file, ready for translation

---
+++ lib/base/pages/about/translations/about_localizations_am.dart
@@ -0,0 +1,45 @@
+import 'about_localizations.dart';
+
+/// The translations for Amharic (`am`).
+class AboutLocalizationAm extends AboutLocalization {
+  AboutLocalizationAm([String locale = 'am']) : super(locale);
+
+  @override
+  String get aboutCollapseContent => 'AddisMap, Ethiopia collected hundreds of bus- and minibus-routes in Addis Ababa and added them to OpenStreetMap to make them usable for the general public. This app is based on the work for Trufi Association. Trufi Association is an international NGO that promotes easier access to public transport. Our apps help everyone find the best way to get from point A to point B within their cities.\n\nIn many cities there are no official maps, routes, apps or timetables. So we compile the available information, and sometimes even map routes from scratch working with local people who know the city. An easy-to-use transportation system contributes to greater sustainability, cleaner air and a better quality of life.';
+
+  @override
+  String get aboutCollapseContentFoot => 'We need mappers, developers, planners, testers, and many other hands.';
+
+  @override
+  String get aboutCollapseTitle => 'More About AddisMap and Trufi Association';
+
+  @override
+  String aboutContent(Object appName) {
+    return 'Need to go somewhere and don\'t know which minibus or bus to take?\nThe $appName App makes it easy!\n\nAddisMap is team from Ethiopia and beyond. We love minibuses and public transportation, and we want to make it easier to use for everyone. So we developed this app. We hope you enjoy it.';
+  }
+
+  @override
+  String get aboutLicenses => 'ፈቃዶች';
+
+  @override
+  String get aboutOpenSource => 'ይህ መተግበሪያ በ GitHub ላይ እንደ ክፍት ምንጭ ተለቅቋል። ወደ የራስዎ ከተማ ለማበርከት ወይም ለማምጣት ነፃነት ይሰማዎ።';
+
+  @override
+  String get menuAbout => 'ስለ';
+
+  @override
+  String tagline(Object city) {
+    return 'በ$city የብዙሀን ትራንስፖርት';
+  }
+
+  @override
+  String get trufiWebsite => 'የአዲስማፕ ትራንዚት ድረ-ገፅ';
+
+  @override
+  String version(Object version) {
+    return 'Version $version';
+  }
+
+  @override
+  String get volunteerTrufi => 'አዲስማፕ ትራንዚትን በጎ ፈቃደኛ ሆነው ይቀላቀሉ።';
+}
